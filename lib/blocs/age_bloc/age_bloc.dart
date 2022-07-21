fa847361a463bfa93af3ec79de31d99d81346a94
ahmedtemitopeai <ahmedtemitopeai@gmail.com>
2022-07-21 09:59:11 +0100

    Age Bloc updated with the hydrated bloc package to persist and restore bloc states

--- lib/blocs/age_bloc/age_bloc.dart
+++ lib/blocs/age_bloc/age_bloc.dart
@@ -1,14 +1,25 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 part 'age_event.dart';
 
-class AgeBloc extends Bloc<AgeEvent, int> {
+class AgeBloc extends HydratedBloc<AgeEvent, int> {
   AgeBloc() : super(0) {
     on<AgeValueChanged>(_mapAgeValueChangedToState);
   }
 
   void _mapAgeValueChangedToState(AgeValueChanged event, Emitter<int> emit) {
     emit(event.ageValue);
+  }
+
+  @override
+  int? fromJson(Map<String, dynamic> json) {
+    return json['state'];
+  }
+
+  @override
+  Map<String, dynamic>? toJson(int state) {
+    return {'state': state};
   }
 }
